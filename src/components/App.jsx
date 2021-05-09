7925211e5b76f2fa64aad721448e6bfb577087a2
hemanth253 <83533561+hemanth253@users.noreply.github.com>
2021-05-09 17:08:31 +0530

    v1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,15 @@
 import React from "react";
 
-function App() {
+import contacts from "../contacts";
+import Card from "./Card";
+
+function App(props) {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
-      <div className="card">
-        <div className="top">
-          <h2>Beyonce</h2>
-          <img
-            src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>+123 456 789</p>
-          <p>b@beyonce.com</p>
-        </div>
-      </div>
+      <Card details={contacts[0]} />
+      <Card details={contacts[1]} />
+      <Card details={contacts[2]} />
     </div>
   );
 }
